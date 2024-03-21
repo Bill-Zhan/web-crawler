7edaa9d9865f7ab4d882d4d569e4858a1174a44c
Xiaotian Zhan <xizhan@linkedin.com>
2024-03-20 17:45:32 -0700

    [Update] add sender_email and sender_pwd to parameters

--- perm-processing-time/perm_flow.py
+++ perm-processing-time/perm_flow.py
@@ -9,13 +9,21 @@ from info_email import inform_processing_time
 cron_schedule = construct_schedule(cron="0 9 * * 1,4", timezone="America/Los_Angeles")  # At 09:00 am on Monday and Thursday, US west time
 
 @flow(log_prints=True)
-def daily_inform_processing_time_flow(receiving_email: str):
-    inform_processing_time(receiving_email)
+def daily_inform_processing_time_flow(
+    sender_email: str,
+    sender_pwd: str,
+    receiving_email: str
+):
+    inform_processing_time(sender_email, sender_pwd, receiving_email)
 
 if __name__ == "__main__":
     daily_inform_processing_time_flow.serve(
         name="daily-inform-perm-processing-time",
         tags=["daily", "crawl"],
-        parameters={"receiving_email": "billtsan1994@gmail.com"},
+        parameters={
+            "sender_email": "",
+            "sender_pwd": "",
+            "receiving_email": "billtsan1994@gmail.com"
+        },
         schedule=cron_schedule,
     )
