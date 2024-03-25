b9ea53e2ecf2a0756a9e00aa8381da348cc9470f
Xiaotian Zhan <xizhan@linkedin.com>
2024-03-24 21:40:22 -0700

    [Fix] remove json reading

--- perm-processing-time/perm_flow.py
+++ perm-processing-time/perm_flow.py
@@ -1,21 +1,12 @@
 """
 Script to web crawl perm processing time
 """
-import json
 from prefect import flow
 from prefect.client.schemas.schedules import construct_schedule
 from info_email import inform_processing_time
 
 # Define the schedule
 cron_schedule = construct_schedule(cron="0 9 * * 1,4", timezone="America/Los_Angeles")  # At 09:00 am on Monday and Thursday, US west time
-
-# Define function to get account and password
-def read_email_config(file_path="email_pwd.json"):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    return data["send_email"], data["send_email_pwd"]
-
-sender_email, sender_pwd = read_email_config()
 
 @flow(log_prints=True)
 def daily_inform_processing_time_flow(
@@ -35,9 +26,9 @@ if __name__ == "__main__":
         job_variables={"pip_packages": ["bs4", "requests"]},
         tags=["daily", "crawl"],
         parameters={
-            "sender_email": sender_email,
-            "sender_pwd": sender_pwd,
-            "receiving_email": "billtsan1994@gmail.com"
+            "sender_email": "",  # to set in flow
+            "sender_pwd": "",  # to set in flow
+            "receiving_email": "", # to set in flow
         },
         schedule=cron_schedule,
     )
